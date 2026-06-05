93613220d259df54f6f712d949ff8ba50d7266f1
victorysingh <sarshvinder180@gmail.com>
2026-06-05 19:45:45 +0530

    Implement fatigue label collection

---
+++ collector/fatigue_logger.py
@@ -0,0 +1,36 @@
+import uuid
+
+from database.db import insert_fatigue_label
+
+session_id = str(uuid.uuid4())[:8]
+
+
+def start_fatigue_logger():
+
+    while True:
+
+        try:
+
+            fatigue_level = int(
+                input(
+                    "\nEnter fatigue level (1-10): "
+                )
+            )
+
+            if fatigue_level < 1 or fatigue_level > 10:
+                print("Please enter a value between 1 and 10")
+                continue
+
+            insert_fatigue_label(
+                session_id,
+                fatigue_level
+            )
+
+            print(
+                f"Saved fatigue level: {fatigue_level}"
+            )
+
+            break
+
+        except ValueError:
+            print("Enter a valid number")
